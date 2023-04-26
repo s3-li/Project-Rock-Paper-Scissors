48c62aab88c41b53577fb7aa9e8167ad86636cc0
s3-li <124447576+s3-li@users.noreply.github.com>
2023-04-26 17:39:44 +0100

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -5,5 +5,19 @@ const items = ["Rock", "Paper", "Scissors"]
 function getComputerChoice() {
   return items[Math.floor(Math.random()*items.length)];
 }
-console.log(getComputerChoice(items))
 
+// Create function to play a single round of Rock Paper Scissors.
+const computerSelection = getComputerChoice();
+const playerSelection = "rock"
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "Paper") {
+  return "Sorry, you lose this time. Paper beats Rock."
+  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+  return "Congratulations, you win! Rock beats Scissors."
+  } else if (playerSelection === "rock" && computerSelection === "Rock") {
+  return "It's a Draw!"
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
