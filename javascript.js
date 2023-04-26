ffe7b106edffcc28c1b268560a6031b2ead00a81
s3-li <124447576+s3-li@users.noreply.github.com>
2023-04-26 17:03:27 +0100

    Create function to randomly pick an item

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+// List random items.
+const items = ["Rock", "Paper", "Scissors"]
+
+// Create function to generate random item as the computer's choice.
+function getComputerChoice() {
+  return items[Math.floor(Math.random()*items.length)];
+}
+console.log(getComputerChoice(items))
+
