5aa315adc61e7f63d609d6155ddb6ced9e34fbd1
s3-li <124447576+s3-li@users.noreply.github.com>
2023-04-27 17:38:49 +0100

    Add toLowerCase method to playerSelection argument to account for case-sensitivity. Edit  conditionals in playRound function to account for all selections

--- javascript.js
+++ javascript.js
@@ -1,23 +1,28 @@
-// List random items.
-const items = ["Rock", "Paper", "Scissors"]
+// Array list of items.
+const items = ["rock", "paper", "scissors"];
 
-// Create function to generate random item as the computer's choice.
+
+// Create function to generate random item from the list as the computer's choice.
 function getComputerChoice() {
   return items[Math.floor(Math.random()*items.length)];
 }
 
+
 // Create function to play a single round of Rock Paper Scissors.
 const computerSelection = getComputerChoice();
-const playerSelection = "rock"
+const playerSelection = "rock";
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "Paper") {
-  return "Sorry, you lose this time. Paper beats Rock."
-  } else if (playerSelection === "rock" && computerSelection === "Scissors") {
-  return "Congratulations, you win! Rock beats Scissors."
-  } else if (playerSelection === "rock" && computerSelection === "Rock") {
-  return "It's a Draw!"
+  if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
+    return "You win!";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
+    return "You win!";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
+    return "You win!";
+  } else if (playerSelection.toLowerCase() === computerSelection) {
+    return "It's a draw.";
+  } else {
+    return "You lose.";
   }
 }
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection))
